80109653f37299902a3625e55208ad05e1469f23
Egor Zheliba <egor.anoshin.03@mail.ru>
2023-09-05 20:03:24 +0300

    Initial Commit

---
+++ First_Sem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  First_Sem
+//
+//  Created by Egor Anoshin on 05.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
